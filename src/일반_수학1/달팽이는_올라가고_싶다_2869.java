8359903dbbb71f3d9bd57338095040920694488f
JeongBin0462 <dlwjdqls783@gmail.com>
2024-01-10 20:35:05 +0900

    +1문제

--- src/일반_수학1/달팽이는_올라가고_싶다_2869.java
+++ src/일반_수학1/달팽이는_올라가고_싶다_2869.java
@@ -1,8 +1,10 @@
+package 일반_수학1;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class 달팽이는_올라가고_싶다_2869 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] strArr = br.readLine().split(" ");
