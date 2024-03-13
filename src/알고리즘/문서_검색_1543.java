4cc29dad94bae5154479c07aea5b9fcf3b895469
JeongBin0462 <dlwjdqls783@gmail.com>
2024-03-13 18:50:30 +0900

    +1문제

--- src/알고리즘/문서_검색_1543.java
+++ src/알고리즘/문서_검색_1543.java
@@ -1,6 +1,8 @@
+package 알고리즘;
+
 import java.util.Scanner;
 
-public class Main {
+public class 문서_검색_1543 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
