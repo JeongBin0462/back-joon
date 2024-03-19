d3660be774dc6cf0b67df3cc2d7d778d0a3e5e90
JeongBin0462 <dlwjdqls783@gmail.com>
2024-03-19 19:57:30 +0900

    +1문제

--- src/알고리즘/소금폭탄_13223.java
+++ src/알고리즘/소금폭탄_13223.java
@@ -1,6 +1,8 @@
+package 알고리즘;
+
 import java.util.Scanner;
 
-class Main {
+public class 소금폭탄_13223 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] current = sc.next().split(":");
